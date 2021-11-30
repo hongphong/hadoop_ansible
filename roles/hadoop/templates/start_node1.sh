d59cb754120a8a8f2397ee5918755e11a9feb3c2
Phong Pham <phongbro1805@gmail.com>
2021-11-30 10:22:36 +0700

    add file start service

--- roles/hadoop/templates/start_node1.sh
+++ roles/hadoop/templates/start_node1.sh
@@ -1,7 +1,6 @@
 cd {{ hadoop_path }}/hadoop-{{hadoop_version}} &&
 ./sbin/hadoop-daemon.sh start namenode && \
 ./sbin/hadoop-daemon.sh start datanode && \
-./sbin/yarn-daemon.sh start resourcemanager && \
 ./sbin/yarn-daemon.sh start nodemanager  && \
 cd {{ zookeeper_path }}/apache-zookeeper-{{ zookeeper_version }}-bin && \
 ./bin/zkServer.sh start &
