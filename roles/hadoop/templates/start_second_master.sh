d4c0aa7008397e1caf1d36d13de15142c1787444
Phong Pham <phongbro1805@gmail.com>
2021-11-30 10:05:05 +0700

    add file start service

--- roles/hadoop/templates/start_second_master.sh
+++ roles/hadoop/templates/start_second_master.sh
@@ -1,4 +1,4 @@
-./{{ hadoop_path }}/hadoop-{{hadoop_version}}/sbin/hadoop-daemon.sh start secondarynamenode 
-&& ./{{ hadoop_path }}/hadoop-{{hadoop_version}}/sbin/hadoop-daemon.sh start datanode 
-&& ./{{ hadoop_path }}/hadoop-{{hadoop_version}}/sbin/yarn-daemon.sh start nodemanager  
-&& ./{{ zookeeper_path }}/apache-zookeeper-{{ zookeeper_version }}-bin/bin/zkServer.sh start &
+.{{ hadoop_path }}/hadoop-{{hadoop_version}}/sbin/hadoop-daemon.sh start secondarynamenode 
+&& .{{ hadoop_path }}/hadoop-{{hadoop_version}}/sbin/hadoop-daemon.sh start datanode 
+&& .{{ hadoop_path }}/hadoop-{{hadoop_version}}/sbin/yarn-daemon.sh start nodemanager  
+&& .{{ zookeeper_path }}/apache-zookeeper-{{ zookeeper_version }}-bin/bin/zkServer.sh start &
