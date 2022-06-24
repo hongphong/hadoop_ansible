78319032a49d0c53b8a82aa5827804feb500fdc8
Phong <phongbro1805@gmail.com>
2022-06-24 16:21:50 +0700

    config on ubuntu

--- roles/hadoop/templates/start_node1.sh
+++ roles/hadoop/templates/start_node1.sh
@@ -1,6 +1,4 @@
 cd {{ hadoop_path }}/hadoop-{{hadoop_version}} &&
 ./sbin/hadoop-daemon.sh start namenode && \
 ./sbin/hadoop-daemon.sh start datanode && \
-./sbin/yarn-daemon.sh start nodemanager  && \
-cd {{ zookeeper_path }}/apache-zookeeper-{{ zookeeper_version }}-bin && \
-./bin/zkServer.sh start &
+./sbin/yarn-daemon.sh start nodemanager &
--- roles/hadoop/templates/start_node2.sh
+++ roles/hadoop/templates/start_node2.sh
@@ -2,6 +2,4 @@ cd {{ hadoop_path }}/hadoop-{{hadoop_version}} &&
 ./sbin/hadoop-daemon.sh start secondarynamenode && \
 ./sbin/hadoop-daemon.sh start datanode && \
 ./sbin/yarn-daemon.sh start resourcemanager && \
-./sbin/yarn-daemon.sh start nodemanager  && \
-cd {{ zookeeper_path }}/apache-zookeeper-{{ zookeeper_version }}-bin && \
-./bin/zkServer.sh start &
+./sbin/yarn-daemon.sh start nodemanager  &
